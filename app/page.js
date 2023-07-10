dbca6e4d9ae43e42f3efaf4823b8ef7578031aa0
elitemind12 <makyaodaniel55@gmail.com>
2023-07-10 07:24:40 -0700

    master: adding styles to Hero Component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Hero from '../componets/Hero';
-// import Image from 'next/image'
+import Hero from './componets/Hero';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-      <Hero />
+      <Hero Heading="Makyao Photography" Message="I capture moments in nature and keep them alive" />
   )
 }
