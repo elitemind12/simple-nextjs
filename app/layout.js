a22da1303566c25ccf88faa84b7f08319b47d1af
elitemind12 <makyaodaniel55@gmail.com>
2023-07-11 08:48:08 -0700

    master: add NavBar Section_

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 // import { Inter } from 'next/font/google'
-
+import NavBar from './componets/NavBar'
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
