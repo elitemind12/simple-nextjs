a477582caee0ac75e9772246b967c83d3702b14b
elitemind12 <makyaodaniel55@gmail.com>
2023-07-12 08:40:50 -0700

    master: refactor page.js add Slider Component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Hero from './componets/Hero';
-import Image from 'next/image'
+import Slider from './componets/Slider';
+import { sliderData } from './componets/SliderData';
 
 export default function Home() {
   return (
+    <div>
       <Hero Heading="Makyao Photography" Message="I capture moments in nature and keep them alive" />
+      <Slider slides={sliderData} />
+    </div>
   )
 }
