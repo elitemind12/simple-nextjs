a270cd42f31bdb4bd27b7764411461372197e9c6
elitemind12 <makyaodaniel55@gmail.com>
2023-07-12 08:40:00 -0700

    master: refactor Hero.jsx styles

--- app/componets/Hero.jsx
+++ app/componets/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = ({Heading, Message}) => {
     return(
-    <div className='flex items-center justify-center mb-[12] h-screen bg-fixed bg-center bg-cover custom-img'>
+    <div className='flex items-center justify-center mb-12 h-screen bg-fixed bg-center bg-cover custom-img'>
         {/* overlay */}
           <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
           <div className='p-5 text-white z-[2]'>
