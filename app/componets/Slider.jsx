5054990e3b37ee2714d23d1fdc961a23bc9275e5
elitemind12 <makyaodaniel55@gmail.com>
2023-07-12 08:58:42 -0700

    master: add prev&next func to Slider.jsx, add styles to Slider.jsx

--- app/componets/Slider.jsx
+++ app/componets/Slider.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+'use client';
+import React, {useState} from "react";
 import Image from "next/image";
 import { sliderData } from "./SliderData";
 
 const Slider = ({slides}) => {
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0){
+        return null;
+    }
     return(
         <div id='gallery'>
           <h1>Gallery</h1>
           <div>
             {sliderData.map((slide, index) => {
                 return (
-                    <Image src={slide.image} alt='/' width='1440' height='600' objectFit='cover' />
+                    <div key={index} className={index === current ? 'opacity-[1] ease-in duration-1000' : 'opacity-0'}>
+                      {index === current && (
+                        <Image src={slide.image} alt='/' width='1440' height='600' objectFit='cover' />
+                      )}
+                    </div>
                 )
             })}
           </div>
