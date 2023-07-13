99540b6b0971fcebd1d221e3927757dae8714ac5
elitemind12 <makyaodaniel55@gmail.com>
2023-07-13 03:34:04 -0700

    master: create app/componets/Contacts.jsx file add app/componets/Contacts.jsx component to   app/contact/page.jsx add Hero.jsx compnent to   app/contact/page.jsx style app/componets/Contacts.jsx file

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../componets/Hero';
+import Contacts from '../componets/Contacts';
 
 const Contact = () => {
   return (
-    <div>page</div>
+    <div>
+        <Hero Heading='Contact' Message='submit the form below for more works and quotes.' />
+        <Contacts />
+    </div>
   )
 }
 
