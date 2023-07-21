7018bb0fcaba4c3c9e748a870266ccf7fa4c3836
elitemind12 <makyaodaniel55@gmail.com>
2023-07-20 22:44:18 -0700

    refactor

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1437656432973757"
+       crossorigin="anonymous"></script>
+      </head>
       <body>
       <NavBar />
         {children}
